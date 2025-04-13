f33010710e13e99ada1bb66571263af6a891a007
Allison Brown <abrown33914@gmail.com>
2025-04-13 01:43:37 -0400

    made settings page frontend

--- frontend/src/navigation/AuthNavigator.js
+++ frontend/src/navigation/AuthNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import SettingScreen from '../screens/SettingScreen';
 // import TabNavigator from './TabNavigator';
 
 const Stack = createNativeStackNavigator();
@@ -9,6 +10,7 @@ const Stack = createNativeStackNavigator();
 export default function AuthNavigator() {
      return (
           <Stack.Navigator screenOptions={{ headerShown: false }}>
+               <Stack.Screen name="Settings" component={SettingScreen} />
                <Stack.Screen name="Login" component={LoginScreen} />
                <Stack.Screen name="Register" component={RegisterScreen} />
                {/* <Stack.Screen name="Tabs" component={TabNavigator} /> */}
